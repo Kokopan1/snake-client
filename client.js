ea3d55c5a5362484e9c16f4c158b6d70d4dfe0a3
Ashley <ashjansen8@gmail.com>
2024-06-14 16:13:08 -0400

    repeated with set timer and interval

--- client.js
+++ client.js
@@ -8,20 +8,82 @@ const connect = function() {
   });
 
    
-  // Client send a Reply
+  // Register the 'connect' event handler
   conn.on("connect", () => {
-    conn.write("Hello from client!");
+    console.log("Successfully connectd to game server!");
+    // send name to the server
+    conn.write('Name: Ash')
+  });
+
+  conn.on("connect", () => {
+    // send name to the server
+    conn.write('Move: up')
   });
   
+  /* Using Interval
+  set up the interval
+  conn.on("connect", () => {
+
+    conn.write("Move: up")
+
+    setInterval(() => {
+      conn.write("Move: up")
+    }, 1000)
+  });
+  // stop the interval
+*/
+
+
+
+  /* using setTimeout
+  conn.on("connect", () => {
+    // send name to the server
+    conn.write("Move: up")
+
+    // We nested it here but we could also create a new conn.on
+    setTimeout(() => {
+      conn.write("Move: up")
+    }, 1000)
+  });
+  */
+  
+  
+  /* using setTimeout to move snake
+  conn.on("connect", () => {
+    setTimeout(() => {
+      conn.write("Move: up")
+    }, 1500)    
+  })
+
+  conn.on("connect", () => {
+    setTimeout(() => {
+      conn.write("Move: up")
+    }, 2000)    
+  })
+
+  conn.on("connect", () => {
+    setTimeout(() => {
+      conn.write("Move: up")
+    }, 2500)    
+  })
+  */
+
+  /*
+conn.on("connect", () => {
+    setTimeout ( () => {
+      conn.write("Move: up")
+    }, 1000)
+  */
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   //Recieving Data from Clients
   conn.on('data', (data) => {
-    console.log('Name:ASH', data);
+    console.log(data);
   });
-
+ 
   return conn;
 };
+
 
 module.exports = connect;
