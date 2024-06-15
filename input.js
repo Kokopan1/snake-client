6b97f67d4cc82cb75ab85fab602270ffe3adc4dc
Ashley <ashjansen8@gmail.com>
2024-06-14 23:38:39 -0400

    added connections

--- input.js
+++ input.js
@@ -5,21 +5,22 @@ const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
 
-  stdin.on('data', (data) => {
-    if( data === w ) {
-      console.log("Move: up");
-    } else if (data === a) {
-      console.log("Move: left");
-    } else if (data === s) {
-      console.log("Move: down");
-    } else if (data === d) {
-      console.log("Move: right");
-    }
-  })
-  
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
   stdin.resume(); // resume stdin so the program can listen for input
+  
+  process.stdin.on('data', (data) => {
+    if(data === 'w') {
+      connection.write("Move: up");
+    } else if (data === 'a') {
+      connection.write("Move: left");
+    } else if (data === 's') {
+      connection.write("Move: down");
+    } else if (data  === 'd') {
+      connection.write("Move: right");
+    }
+
+  });
   stdin.on("data", handleUserInput);
 
   return stdin;   // return the stdin object so we can use it elsewhere in the program
@@ -31,5 +32,8 @@ const handleUserInput = function (key) {
     process.exit();
   }
 };
+
+
+
 
 module.exports = setupInput;
